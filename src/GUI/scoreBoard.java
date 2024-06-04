64cfc140ef69c1ea56026f48881ab332da251c00
Ben <339710097@gotvdsb.ca>
2024-06-04 13:15:13 +0000

    scoreboard check

--- src/GUI/scoreBoard.java
+++ src/GUI/scoreBoard.java
@@ -2,6 +2,27 @@ package GUI;
 import hsa2.GraphicsConsole;
 import java.awt.*;
 
+import Main.GamePanel;
+
 public class scoreBoard {
 
+    Rectangle r;
+    Color transparentWhite = new Color(255, 255, 255, 40);
+    int score = 0;
+   
+    public scoreBoard() {
+        r = new Rectangle(100, 200, 100,200);
+
+    }
+
+    public void draw() {
+        GamePanel.gc.setColor(transparentWhite);
+        GamePanel.gc.fillRect(r.x, r.y, r.width, r.height);
+    }
+
+    public void update() {
+
+    }
+
 }
+
