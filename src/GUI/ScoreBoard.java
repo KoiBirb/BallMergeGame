f18009b6649a45b62080a35d7fe76acdfaccafeb
KoiBirb <centralleo474@gmail.com>
2024-06-04 09:37:55 -0400

    fixed score drawing

--- src/GUI/ScoreBoard.java
+++ src/GUI/ScoreBoard.java
@@ -2,25 +2,23 @@ package GUI;
 import java.awt.*;
 
 import Main.GamePanel;
-import java.text.ParseException;
 
-public class scoreBoard {
+public class ScoreBoard {
 
     Rectangle r;
     Color transparentWhite = new Color(255, 255, 255, 40);
     int score = 0;
     String scoreDisplay = String.valueOf(score);
    
-    public scoreBoard() {
+    public ScoreBoard() {
         r = new Rectangle(100, 200, 100,200);
-
     }
 
     public void draw() {
         GamePanel.gc.setColor(transparentWhite);
         GamePanel.gc.fillRect(r.x, r.y, r.width, r.height);
         GamePanel.gc.drawOval(100, 100, 70, 70);
-        GamePanel.gc.drawString(score, 130, 145);
+        GamePanel.gc.drawString(scoreDisplay, 130, 145);
     }
 
     public void update() {
