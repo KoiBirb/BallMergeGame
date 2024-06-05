234d3cf1563f0db24340cba0e6361854c9b7bd5f
bencsweir <339710097@gotvdsb.ca>
2024-06-05 09:30:10 -0400

    scoreboar

--- src/GUI/ScoreBoard.java
+++ src/GUI/ScoreBoard.java
@@ -3,33 +3,27 @@ import java.awt.*;
 import java.awt.FontMetrics;
 import Main.GamePanel;
 
-public class ScoreBoard {
+public class ScoreBoard extends FontMetrics{
 
     Rectangle r;
     Color transparentWhite = new Color(255, 255, 255, 40);
     int score = 0;
     String scoreDisplay = String.valueOf(score);
-    Font font = new Font("Arial", Font.PLAIN, 12);
-    FontMetrics metrics = g.getFontMetrics(font);
    
     public ScoreBoard() {
-        r = new Rectangle(100, 200, 100,200);
+        super(new Font("Arial", Font.BOLD, 40));
+        r = new Rectangle(25, 200, 300,400);
     }
 
     public void draw() {
-        GamePanel.gc.setColor(transparentWhite);
+        GamePanel.gc.setColor(Color.white);
         GamePanel.gc.fillRect(r.x, r.y, r.width, r.height);
-        GamePanel.gc.drawOval(100, 100, 70, 70);
-        GamePanel.gc.drawString(scoreDisplay, 130, 145);
+        GamePanel.gc.drawOval(100, 40, 150, 150);
+        GamePanel.gc.setColor(transparentWhite);
+        GamePanel.gc.fillOval(100, 40, 150, 150);
+        GamePanel.gc.setFont (font);
+        GamePanel.gc.drawString(scoreDisplay, 50 - (stringWidth(scoreDisplay))/2, 100);
 
-        /*
-         String str = "Hello, World!";
-
-            // Get the width of the string
-            int width = metrics.stringWidth(str);
-
-            System.out.println("The width of the string is: " + width + " pixels.");
-         */
 
     }
 
