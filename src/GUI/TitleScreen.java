3307891687b4937aec1525dfff656d84e76b5f26
marianats <363406521@gotvdsb.ca>
2024-06-05 09:30:02 -0400

    title screen

--- src/GUI/TitleScreen.java
+++ src/GUI/TitleScreen.java
@@ -8,12 +8,12 @@ import java.util.Objects;
 
 public class TitleScreen {
 
+    //variables and class initializing
     private final int SLEEPTIME = 5;
-    Rectangle button;
+    Rectangle button, help;
     BufferedImage image;
-
-    // initialize classes
     public static final GraphicsConsole gc = new GraphicsConsole(1200, 650);
+    Color string = new Color (160, 82, 45, 50); //transparent color for buttons
 
     // set default settings
     public TitleScreen() {
@@ -22,16 +22,18 @@ public class TitleScreen {
         gc.enableMouseMotion();
         gc.enableMouse();
         gc.setTitle("SUIKA GAME");
-        gc.setBackgroundColor(Color.decode("#eab676"));
+        gc.setBackgroundColor(Color.decode("#eab676")); //incase the image doesnt work
         gc.clear();
 
-        try {
+        try { //import image
             image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Assets/GUI/TitleScreen.jpg")));
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        button = new Rectangle(454, 425, 301, 60);
+        button = new Rectangle(454, 425, 301, 63); //button to get into the game
+        help = new Rectangle( 948, 567, 225, 60); //button to get help
+
     }
 
     /**
@@ -68,9 +70,29 @@ public class TitleScreen {
                 gc.clearRotation();
 
                 gc.drawImage(image,0,0);
-                gc.setColor(Color.GREEN);
-                gc.drawRect(button.x, button.y, button.width, button.height);
+                //gc.setColor(buttons);
+                //gc.drawRect(button.x, button.y, button.width, button.height);
+                //gc.drawRect(help.x, help.y, help.width, help.height);
 
+                if (help.contains(gc.getMouseX(), gc.getMouseY())) {    //check if mouse hovers over help
+                    gc.setColor(Color.WHITE);
+                   // gc.fillRect(760, 370, 300, 175);
+
+                    Polygon info = new Polygon(); //thought bubble for text
+                    info.addPoint(760, 370); //1
+                    info.addPoint(1075, 370); //2
+                    info.addPoint(1075, 545); //3
+                    info.addPoint(1080, 565); //tip
+                    info.addPoint(1050, 545); //tip
+                    info.addPoint(760, 545);//4
+                    gc.fillPolygon(info);
+
+                    //draw string
+                    gc.setColor(string);
+                    gc.setFont( new Font( "Serif", Font.BOLD, 18) );
+                    gc.drawString("String 1", 765,380);
+
+                }
 
             }
         }
