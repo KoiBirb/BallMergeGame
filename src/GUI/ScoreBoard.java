cc30e74b200c59c3ae62f729fc4efb956d6cab35
bencsweir <339710097@gotvdsb.ca>
2024-06-06 09:29:29 -0400

    scoreboar

--- src/GUI/ScoreBoard.java
+++ src/GUI/ScoreBoard.java
@@ -7,7 +7,7 @@ public class ScoreBoard{
 
     Rectangle r;
     Color transparentWhite = new Color(255, 255, 255, 40);
-    int score = 0;
+    int score = 10000;
     String scoreDisplay = String.valueOf(score);
     Font font = new Font("Arial", Font.BOLD, 40);
     FontMetrics fm = GamePanel.gc.getFontMetrics(font);
@@ -18,14 +18,20 @@ public class ScoreBoard{
     }
 
     public void draw() {
-        GamePanel.gc.setColor(Color.white);
+        GamePanel.gc.setColor(new Color(248, 229, 187, 95));
         GamePanel.gc.fillRect(r.x, r.y, r.width, r.height);
+        GamePanel.gc.setStroke(5);
+        GamePanel.gc.setColor(new Color(255, 240,201));
+        GamePanel.gc.drawRect(r.x, r.y, r.width, r.height);
+        GamePanel.gc.setColor(Color.white);
         GamePanel.gc.drawOval(100, 40, 150, 150);
         GamePanel.gc.setColor(transparentWhite);
         GamePanel.gc.fillOval(100, 40, 150, 150);
         GamePanel.gc.setFont (font);
-        GamePanel.gc.drawString(scoreDisplay, 50 - (fm.stringWidth(scoreDisplay))/2, 100);
-
+        GamePanel.gc.drawString(scoreDisplay, 175 - (fm.stringWidth(scoreDisplay))/2, 130);
+        GamePanel.gc.setColor(Color.white);
+        GamePanel.gc.drawString("TOP SCORES", 55, 240);
+        GamePanel.gc.fillRect(60, 260,230, 40);
 
     }
 
