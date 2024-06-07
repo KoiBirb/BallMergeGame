3be11b6664ddc4231b376d72a3ee7b70ab18cabe
marianats <363406521@gotvdsb.ca>
2024-06-07 08:27:08 -0400

    title screen

--- src/GUI/ScoreBoard.java
+++ src/GUI/ScoreBoard.java
@@ -6,7 +6,9 @@ import Main.GamePanel;
 public class ScoreBoard{
 
     Rectangle r;
-    Color transparentWhite = new Color(255, 255, 255, 40);
+    //Color transparentWhite = new Color(255, 255, 255, 40);
+    Color creme = new Color(255, 240,201);
+    Color transparentC = new Color(248, 229, 187, 95);
     int score = 10000;
     String scoreDisplay = String.valueOf(score);
     Font font = new Font("Arial", Font.BOLD, 40);
@@ -18,18 +20,18 @@ public class ScoreBoard{
     }
 
     public void draw() {
-        GamePanel.gc.setColor(new Color(248, 229, 187, 95));
+        GamePanel.gc.setColor(transparentC);
         GamePanel.gc.fillRect(r.x, r.y, r.width, r.height);
         GamePanel.gc.setStroke(5);
-        GamePanel.gc.setColor(new Color(255, 240,201));
+        GamePanel.gc.setColor(creme);
         GamePanel.gc.drawRect(r.x, r.y, r.width, r.height);
-        GamePanel.gc.setColor(Color.white);
+        GamePanel.gc.setColor(creme);
         GamePanel.gc.drawOval(100, 40, 150, 150);
-        GamePanel.gc.setColor(transparentWhite);
+        GamePanel.gc.setColor(transparentC);
         GamePanel.gc.fillOval(100, 40, 150, 150);
         GamePanel.gc.setFont (font);
         GamePanel.gc.drawString(scoreDisplay, 175 - (fm.stringWidth(scoreDisplay))/2, 130);
-        GamePanel.gc.setColor(Color.white);
+        GamePanel.gc.setColor(creme);
         GamePanel.gc.drawString("TOP SCORES", 55, 240);
         GamePanel.gc.fillRect(60, 260,230, 40);
 
